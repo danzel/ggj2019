103274c04075cdef6a51ff891cb3b3ebebd6b513
danzel <danzel@localhost.geek.nz>
2019-01-26 10:29:54 +1300

    hooks as sensors sometimes miss events I think

--- src/scenes/hook.ts
+++ src/scenes/hook.ts
@@ -17,6 +17,7 @@ export class Hook {
 
 	showTelegraph() {
 		this.telegraph = this.scene.add.image((this.source.x + this.destination.x) / 2, (this.source.y + this.destination.y) / 2, 'hook-telegraph');
+		//TODO this.telegraph.blendMode = Phaser.BlendModes.COLOR_BURN;
 		this.scene.telegraphGroup.add(this.telegraph);
 		this.telegraph.alpha = 0.2;
 
@@ -38,7 +39,7 @@ export class Hook {
 			this.body = <Matter.Body>this.image.body;
 			(<any>this.body).hook = this;
 
-			this.body.isSensor = true;
+			//this.body.isSensor = true;
 			this.body.frictionAir = 0.05;
 
 			this.image.applyForce(this.destination.clone().subtract(this.source).scale(0.0015));
